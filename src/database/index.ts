a4c6d455f1e4f43560b9f2dcaf13505e42756e7e
Mateus Costa Silva <38388657+matcs06@users.noreply.github.com>
2023-07-11 00:50:22 -0300

    Update index.ts
    updating database name

--- src/database/index.ts
+++ src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'pedefast'
+          ? 'pedefastdeploy'
           : defaultOptions.database,
     }),
   );
